9931efc1403517c24fc5b7b961e2953b3eb26fc0
Jonathan Bruneau <32800587+WhiteMat@users.noreply.github.com>
2023-03-26 23:13:45 +0200

    V1.1 GetSharedFilesACL.ps1
    New arguments : -CSVExport, -HTMLExport, -AllChilds

--- GetSharedFilesACL.ps1
+++ GetSharedFilesACL.ps1
@@ -1,10 +1,21 @@
-﻿$shared_files = Get-WmiObject -Class Win32_Share | Where-Object Type -eq 0
+﻿Param(
+  [Parameter(Mandatory=$false)]
+  [boolean]$CSVExport,
+    
+  [Parameter(Mandatory=$false)]
+  [boolean]$HTMLExport,
+
+  [Parameter(Mandatory=$false)]
+  [boolean]$AllChilds
+)
+
+$shared_files = Get-WmiObject -Class Win32_Share | Where-Object Type -eq 0
 $allHTMLElement = ""
 
 $result = foreach ($shared_file in $shared_files) {
   Get-ChildItem -Path $shared_file.Path –Recurse | Foreach-Object {
     if ($_.Parent.FullName) {
-      if ( (Get-Acl $_.FullName).Owner -ne (Get-Acl $_.Parent.FullName).Owner -or ((Get-Acl $_.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) -ne ((Get-Acl $_.Parent.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) ) {
+      if ( $AllChilds -or (Get-Acl $_.FullName).Owner -ne (Get-Acl $_.Parent.FullName).Owner -or ((Get-Acl $_.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) -ne ((Get-Acl $_.Parent.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) ) {
         $newHTMLElement = '</br><button class="leaflet" id="'+$_.FullName+'">+</button> <b>'+$_.FullName+'</b><div hidden id="_'+$_.FullName+'"><table><thead><tr><th>FileSystemRights</th><th>AccessControlType</th><th>IdentityReference</th><th>IsInherited</th><th>InheritanceFlags</th><th>PropagationFlags</th></tr></thead><tbody>'
         foreach ($acl in (Get-Acl $_.FullName).Access) {
           $newHTMLElement = $newHTMLElement + '
@@ -33,7 +44,7 @@ $result = foreach ($shared_file in $shared_files) {
       $allHTMLElement = $allHTMLElement + $newHTMLElement + '<b>Owner : '+(Get-Acl $_.FullName).Owner+'</b></div>'
     }
     if ($_.Directory.FullName) {
-      if ( (Get-Acl $_.FullName).Owner -ne (Get-Acl $_.Directory.FullName).Owner -or ((Get-Acl $_.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) -ne ((Get-Acl $_.Directory.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) ) {
+      if ( $AllChilds -or (Get-Acl $_.FullName).Owner -ne (Get-Acl $_.Directory.FullName).Owner -or ((Get-Acl $_.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) -ne ((Get-Acl $_.Directory.FullName).Access | Format-List -Property FileSystemRights, AccessControlType, IdentityReference | Out-String) ) {
         $newHTMLElement = '</br><button class="leaflet" id="'+$_.FullName+'">+</button> <b>'+$_.FullName+'</b><div hidden id="_'+$_.FullName+'"><table><thead><tr><th>FileSystemRights</th><th>AccessControlType</th><th>IdentityReference</th><th>IsInherited</th><th>InheritanceFlags</th><th>PropagationFlags</th></tr></thead><tbody>'
         foreach ($acl in (Get-Acl $_.FullName).Access) {
           $newHTMLElement = $newHTMLElement + '
@@ -64,41 +75,47 @@ $result = foreach ($shared_file in $shared_files) {
   }
 }
 
-$time = Get-Date -Format "MMddyyyyHHmm"
-$file = (Get-Location).tostring() + '\' + $time
-$temp = New-item -Path $file -ItemType Directory
-$csv = (Get-Location).tostring() + '\' + $time +'\export.csv'
-$html = (Get-Location).tostring() + '\' + $time +'\index.html'
+if ($CSVExport -or $HTMLExport) {
+  $time = Get-Date -Format "MMddyyyyHHmm"
+  $file = (Get-Location).tostring() + '\' + $time
+  $temp = New-item -Path $file -ItemType Directory
+}
 
-'
-<style>
-  table, td, th {
-    border: 1px solid;
-  }
 
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-</style>
-<h1>File Shared ACL HTML Export</h1>
-'+ $allHTMLElement +'
-<script>
-    const buttons = document.getElementsByClassName("leaflet");
-    for (var i = 0 ; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", (event) => {
-            var toChange = document.getElementById("_" + event.explicitOriginalTarget.id);
-            if (toChange.hidden) {
-                toChange.hidden = false;
-            } else {
-                toChange.hidden = true;
-            }
-        });
-    }
-</script>
-' | Out-File -FilePath $html
-$result | Export-Csv -Path $csv
+if ($HTMLExport) {
+    $html = (Get-Location).tostring() + '\' + $time +'\index.html'
+    '
+    <style>
+      table, td, th {
+        border: 1px solid;
+      }
+
+      table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+    </style>
+    <h1>File Shared ACL HTML Export</h1>
+    '+ $allHTMLElement +'
+    <script>
+        const buttons = document.getElementsByClassName("leaflet");
+        for (var i = 0 ; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", (event) => {
+                var toChange = document.getElementById("_" + event.explicitOriginalTarget.id);
+                if (toChange.hidden) {
+                    toChange.hidden = false;
+                } else {
+                    toChange.hidden = true;
+                }
+            });
+        }
+    </script>
+    ' | Out-File -FilePath $html
+}
+
+if ($CSVExport) {
+    $csv = (Get-Location).tostring() + '\' + $time +'\export.csv'
+    $result | Export-Csv -Path $csv
+}
 
 $result | Format-Table
-Write-Host 'Export HTML écrit à' $html
-Write-Host 'Export CSV écrit à' $csv
--- README.md
+++ README.md
@@ -7,3 +7,12 @@ This script only works in the Windows environment.
 
 - Export CSV
 - Export HTML
+
+### How to use it ?
+To get the raw result, you can simply run the script without arguments:
+
+To get an HTML export, you can use the -HTMLExport argument:
+
+To get a CSV export, you can use the -CSVExport argument:
+
+To get all child files and not only the children that are different from their parents, you can use the -AllChilds argument:
